41690ffb6e285fdd502b4cbe3847944876f3ee2e
abyanmaheswara <om.abyan123gmail.com>
2025-10-23 09:23:45 +0700

    docs: update README.md with Kelola Anggota examples and fix BukuAudio.kt info method

--- src/model/BukuAudio.kt
+++ src/model/BukuAudio.kt
@@ -13,6 +13,15 @@ class BukuAudio(
     override fun info(): String {
         val jam = durasiMenit / 60
         val menit = durasiMenit % 60
-        return "AudioBook: $judul oleh $penulis, durasi ${jam}j ${menit}m, narator: $narrator"
+        return '''
+            Tipe: Buku Audio
+            ID: $id
+            Judul: $judul
+            Penulis: $penulis
+            Tahun: $tahun
+            Kategori: $kategori
+            Durasi: ${jam}j ${menit}m
+            Narator: $narrator
+        '''.trimIndent()
     }
 }
